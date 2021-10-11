031b31743c079299861f1c14aa906df86b86f474
jmParkGit <papk0904@naver.com>
2021-10-11 20:50:46 +0900

    homework3(crawling genie)

--- pythonprac/genie.py
+++ pythonprac/genie.py
@@ -12,4 +12,16 @@ data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&ymd=20200403&hh
 
 soup = BeautifulSoup(data.text, 'html.parser')
 
-print(soup)
+# rank: #body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.number
+# title: #body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.info > a.title.ellipsis
+# singer: #body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.info > a.artist.ellipsis
+
+trs = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
+
+for tr in trs:
+    rank=tr.select_one('td.number').text[0:2].strip();
+    title = tr.select_one('td.info > a.title.ellipsis').text.strip()
+    singer = tr.select_one('td.info > a.artist.ellipsis').text.strip();
+
+    if rank is not None:
+        print(rank,title,singer);
